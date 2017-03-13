da74d1865c67decb7f34cab1b187e3b76238a55a
Alvaro Aguilera <a.aguilera@coolblue.nl>
2017-03-13 14:13:48 +0100

    initial Commit

---
+++ FailoverSpawnSharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace FailoverSpawnSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // call DoWork()
+                Console.WriteLine(DoWork());
+                // Sleep 5 minutes.
+                System.Threading.Thread.Sleep(5 * 60 * 1000);
+            }
+        }
+
+        private static object ExecuteQuery(OracleConnection connection, string query)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            var reader = command.ExecuteReader();
+            try
+            {
+                reader.Read();
+                return reader.GetValue(0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                reader.Close();
+                return 1;
+            }
+        }
+
+        private static string DoWork()
+        {
+            var connectionString = @"Data Source=SPAWN; User Id = " + Environment.GetEnvironmentVariable("spawnusername") + "; Password = " + Environment.GetEnvironmentVariable("spawnpassword") + ";";
+
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+                var dbName = ExecuteQuery(connection, @"select lower(sys_context('userenv','db_name')) as db_name from dual");
+                var sysdate = ExecuteQuery(connection, @"select sysdate from dual");
+                connection.Dispose();
+                connection.Close();
+                OracleConnection.ClearPool(connection);
+                OracleConnection.ClearAllPools();
+                return $"[{sysdate}]: {dbName}";
+            }
+        }
+    }
+}
