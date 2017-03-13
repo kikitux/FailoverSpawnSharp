194f89c9eddca06539eb180cd1fdcaf471cc0303
Alvaro Aguilera <a.aguilera@coolblue.nl>
2017-03-13 15:55:51 +0100

    run twice the staments, and measure

--- FailoverSpawnSharp/Program.cs
+++ FailoverSpawnSharp/Program.cs
@@ -42,18 +42,24 @@ namespace FailoverSpawnSharp
 
             using (OracleConnection connection = new OracleConnection(connectionString))
             {
+                
                 // Create new stopwatch.
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
 
                 connection.Open();         
                 Console.WriteLine("Milliseconds elapsed open connection\t\t: {0}", stopwatch.ElapsedMilliseconds);
-
+               
+                // first run
                 var dbName = ExecuteQuery(connection, @"select lower(sys_context('userenv','db_name')) as db_name from dual");
                 var sysdate = ExecuteQuery(connection, @"select sysdate from dual");
+                Console.WriteLine("Milliseconds elapsed first run staments\t\t: {0}", stopwatch.ElapsedMilliseconds);
 
-                Console.WriteLine("Milliseconds elapsed run staments\t\t: {0}", stopwatch.ElapsedMilliseconds);
-                
+                // second run
+                dbName = ExecuteQuery(connection, @"select lower(sys_context('userenv','db_name')) as db_name from dual");
+                sysdate = ExecuteQuery(connection, @"select sysdate from dual");
+                Console.WriteLine("Milliseconds elapsed second run staments\t: {0}", stopwatch.ElapsedMilliseconds);
+
                 connection.Dispose();
                 connection.Close();
                 OracleConnection.ClearPool(connection);
