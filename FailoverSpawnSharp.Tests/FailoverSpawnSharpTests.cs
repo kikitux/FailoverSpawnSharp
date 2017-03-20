36e362c8f4c47f9d22032ba53e4c743225eb7476
Niels Abels <n.abels@coolblue.nl>
2017-03-20 12:08:27 +0100

    Added two tests that always succeed.

---
+++ FailoverSpawnSharp.Tests/FailoverSpawnSharpTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FailoverSpawnSharp.Tests
+{
+    [TestClass]
+    public class FailoverSpawnSharpTests
+    {
+        [TestMethod]
+        public void TestThatPasses()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestThatAlsoPasses()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
